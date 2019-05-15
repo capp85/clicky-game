51d6fff7017b19da0ec84f79817ee3c1a37507ea
Joseph Serrato <joseph.serrato@gmail.com>
2019-05-15 12:28:26 -0700

    Removed sub folder

---
+++ build/precache-manifest.5080b95a1e2e746714fe616d26bfd386.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae537f0554071204abdf6705ec9a913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc4cf562c1aeb9a1932",
+    "url": "/static/css/main.a831c047.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "dbc4cf562c1aeb9a1932",
+    "url": "/static/js/main.91916a85.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
